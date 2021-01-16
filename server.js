61daf4335a2f98a30162d6cce2eb1d35aa9cba07
Saqib Abbasi <saqib-abbasi@live.com>
2021-01-16 19:55:18 +0500

    added

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express');
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 const bootcamps = require('./Routes/bootcamp');
-
+//const logger = require('./middlewear/logger');
+const morgan = require('morgan');
 dotenv.config({ path: './config/config.env' });
-
-
 const app = express();
 
-const logger = (req, res, next) => {
+//app.use(logger);
 
-     console.log(`Middle wear call ${req.method} ${req.protocol}:${req.get('host')}:${req.orginalURL}`);
-     next();
+
+if (process.env.NODE_ENV === 'development') {
+     app.use(morgan('dev'));
 }
-app.use(logger);
-
 app.use('/api/v1/bootcamps', bootcamps);
-
 const PORT = process.env.PORT || 5000;
-
-
 app.listen(PORT, console.log(`server is listening on ${PORT},it has environmnet ${process.env.NODE_ENV}`));
-
-
